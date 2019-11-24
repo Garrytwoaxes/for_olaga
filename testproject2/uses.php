abb805cc516aab027b6153d8aa28405ac121a9ff
Igor Poritskiy <igorengine@yandex.ru>
2019-11-24 03:12:47 +0300

    Update uses.php

--- testproject2/uses.php
+++ testproject2/uses.php
@@ -4,21 +4,22 @@
         <title> Проверка доменов</title>
     </head>
 <?php
-    require_once 'connect.php';
+    require_once 'connect.php'; // вставляю файл с настройками подключения к базе
     
-    $link = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+    $link = mysqli_connect($host, $dbuser, $dbpass, $dbname); //Подключаюсь  к базе
      
 
     $query =  "SELECT substring(email,LOCATE ('@',email),LENGTH (email)) as domain, count(substring(email,LOCATE('@',email),LENGTH  (email))) as counter  from email group by  domain order by counter DESC" ;
-    $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
+    // делаю запрос включающий валидность, группировку и сортировку
+    $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); //обрабатываю запрос на php 
         if(mysqli_num_rows($result) > 0)
           {
           
-            while($row = mysqli_fetch_assoc($result)) {  
+            while($row = mysqli_fetch_assoc($result)) {  // в цикле перебираю результат обработки запроса
     
-                echo  " " . $row["domain"]. " " .$row["counter"]. "<br>"; 
+                echo  " " . $row["domain"]. " " .$row["counter"]. "<br>"; // вывожу на экран резульат
                 
             }
         }
-    mysqli_close($link);
+    mysqli_close($link); // закрываю дискотеку!
 ?>
